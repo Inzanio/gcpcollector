b1936d726c63c14dfbac4bc4877ab1cd4d4d6a95
LoonerDrive <noa.edouardmichel@gmail.com>
2025-04-18 08:29:00 +0100

    first commit

---
+++ public/db.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+use Kreait\Firebase\Factory;
+
+$GOOGLE_APPLICATION_CREDENTIALS = "../application_default_credentials.json";
+
+$factory = (new Factory())
+     ->withServiceAccount($GOOGLE_APPLICATION_CREDENTIALS);
+  
+
+// $auth = $factory->createAuth();
+// $realtimeDatabase = $factory->createDatabase();
+// $cloudMessaging = $factory->createMessaging();
+// $remoteConfig = $factory->createRemoteConfig();
+// $cloudStorage = $factory->createStorage(); 
+$firestore = $factory->createFirestore();
+$database = $firestore->database();
+echo "Hellow world";
+?>
