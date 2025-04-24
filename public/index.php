4989bfab39e544b83bab1a8464f55df7dee9a779
LoonerDrive <noa.edouardmichel@gmail.com>
2025-04-24 15:22:35 +0100

    ajout de la redirection des pages non désirées de l'application

--- public/index.php
+++ public/index.php
@@ -1,50 +1,18 @@
 <?php
-  //require_once("routes.php");
-  session_start();
-  //var_dump($_SESSION['user_role']);
-  if (!isset($_SESSION['user_role']) ) header('Location: login.php'); 
+    $route = $_GET['route'];
+
+    switch ($route) {
+        case '':
+            require_once 'dashboard.php';
+            break;
+        case 'login':
+            require_once 'login.php';
+            break;
+        // case 'contact':
+        //     include 'contact.php';
+        //     break;
+        default:
+            require_once '404.php';
+            break;
+    }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<?php
-    require_once("pages/head.php");
-?>
-
-<body>
-
-
-  <!-- ======= Header ======= -->
-      <?php
-        require_once("pages/header.php");
-      ?>
-  <!-- End Header -->
-
-  <!-- ======= Sidebar ======= -->
-      <?php
-        
-        require_once("pages/sidebar.php");
-      ?>
-  <!-- End Sidebar-->
-
-  <!-- ======= Main ======= -->
-  <main id="main" class="main">
-      <?php
-      if  ($_SESSION['user_role'] == "agent"){
-        require_once("pages/contentAgent.php");
-      }
-      //elseif($_SESSION['user_role'])
-        
-      ?>
-  </main>
-  <!-- End #main -->
-
-  <!-- ======= Footer ======= -->
-      <?php
-        require_once("pages/footer.php");
-      ?>
-  <!-- End Footer -->
-
-</body>
-
-</html>
