3effb5e9c38a970ce428a84ed3978caf0816a671
Belviane1 <songmenebelviane@gmal.com>
2025-04-24 15:55:39 +0100

    Page error 404

--- public/index.php
+++ public/index.php
@@ -3,16 +3,16 @@
 
     switch ($route) {
         case '':
-            require_once 'dashboard.php';
+            require_once './dashboard.php';
             break;
         case 'login':
-            require_once 'login.php';
+            require_once './login.php';
             break;
         // case 'contact':
         //     include 'contact.php';
         //     break;
         default:
-            require_once '404.php';
+            require_once './404.php';
             break;
     }
 ?>
